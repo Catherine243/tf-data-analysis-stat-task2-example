3a54a3e061dfe10d9c63003f647d9aaa8668f851
Catherine243 <129989270+Catherine243@users.noreply.github.com>
2023-04-06 01:15:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,15 +13,17 @@ def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     t = 65
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    loc = sum(x)/n
+    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    
+    import scipy.stats as st
+    z1 = st.gamma.ppf(1-alpha/2, a = n, scale = 1)
+    z2 = st.gamma.ppf(alpha/2, a = n, scale = 1)
+    return loc/(t*t) + 1/(2*t*t) + z1/(t*t), \
+           loc/(t*t) + 1/(2*t*t) + z2/(t*t)
+
+    
+ #   return loc - scale * norm.ppf(1 - alpha / 2), \
+ #          loc - scale * norm.ppf(alpha / 2)
 
 
-#пока примерно что-то:
-import scipy.stats as st
-z1 = st.gamma.ppf(1-alpha/2, a = n, scale = 1)
-z2 = st.gamma.ppf(alpha/2, a = n, scale = 1)
-loc/(t*t) + 1/(2*t*t) + z1/(t*t), \
-loc/(t*t) + 1/(2*t*t) + z2/(t*t)
