ff2c1e7177973576ae62288e37eb5e08d773b835
Catherine243 <129989270+Catherine243@users.noreply.github.com>
2023-04-07 20:35:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
+import scipy.stats as st
 
 chat_id = 557932710 # Ваш chat ID, не меняйте название переменной
 
@@ -14,16 +13,10 @@ def solution(p: float, x: np.array) -> tuple:
     t = 65
     alpha = 1 - p
     loc = sum(x)/n
-    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+
     
-    import scipy.stats as st
-    z1 = st.gamma.ppf(1-alpha/2, a = n, scale = 1)
-    z2 = st.gamma.ppf(alpha/2, a = n, scale = 1)
+    z1 = st.gamma.ppf((1+p)/2, a = n, scale = 1/n)
+    z2 = st.gamma.ppf((1-p)/2, a = n, scale = 1/n)
+    
     return loc/(t*t) + 1/(2*t*t) + z1/(t*t), \
            loc/(t*t) + 1/(2*t*t) + z2/(t*t)
-
-    
- #   return loc - scale * norm.ppf(1 - alpha / 2), \
- #          loc - scale * norm.ppf(alpha / 2)
-
-
